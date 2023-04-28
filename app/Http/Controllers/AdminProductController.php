67e2dd73c6b0f4aace89b95ff1d9da220f3927de
Waber Júnior <waber.junior@madefy.com.br>
2023-04-28 00:55:44 -0300

    first commit

---
+++ app/Http/Controllers/AdminProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminProductController extends Controller
+{
+    public function index()
+    {
+        return view('admin.products');
+    }
+
+    public function edit()
+    {
+        return view('admin.product_edit');
+    }
+}
